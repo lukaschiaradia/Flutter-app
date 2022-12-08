9ddb05877c6fd33e0bdb7c93704e7fac46334350
tristan-guepeed <tristan.beau@epitech.eu>
2022-12-08 14:37:12 +0100

    //[ADD] initial commit

---
+++ lib/name_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:async';
+import 'delayed_animation.dart';
+import 'main.dart';
+import 'age_page.dart';
+
+class NamePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            DelayedAnimation(
+                delay: 200,
+                child: Container(
+                  height: 100,
+                  margin: EdgeInsets.only(
+                    top: 40,
+                    bottom: 0,
+                  ),
+                  child: Text(
+                    "Quel est votre nom ?",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontSize:25,
+                    )
+                  ),
+                ),
+            ),
+            DelayedAnimation(
+                delay: 300,
+                child: Container(
+                  height: 50,
+                  margin: EdgeInsets.only(
+                    top: 0,
+                    bottom: 0,
+                  ),
+                  child: Text(
+                    "Nom complet",
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.poppins(
+                      color: Color.fromARGB(255, 42, 42, 42),
+                      fontSize:15,
+                    )
+                  ),
+                ),
+            ),
+            DelayedAnimation(
+                delay: 300,
+                child: Container(
+                  height: 50,
+                  margin: EdgeInsets.only(
+                    top: 0,
+                    bottom: 200,
+                  ),
+                  child: Text(
+                    "Andrew Ainsley",
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.poppins(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize:20,
+                    )
+                  ),
+                ),
+            ),
+            DelayedAnimation(
+                delay: 300,
+                child: Container(
+                  height: 50,
+                  margin: EdgeInsets.only(
+                    top: 0,
+                    bottom: 100,
+                  ),
+                  child: Image.asset(
+                    "images/progression1.png",
+                  ),
+                ),
+            ),
+            DelayedAnimation(
+                delay: 500,
+                child: Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: blue_color,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    child: Text("Continuer", textScaleFactor: 1.5,),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AgePage()),
+                      );
+                    },
+                  ),
+                )
+              ),
+          ],
+        ),
+      )
+    );
+  }
+}
